e92280a006b210dcb1bb2d21cbc1ac64581cdf9e
Patrick Dewey <patrickdewey@vt.edu>
2023-11-18 11:03:32 -0500

    v2

--- profile.sh
+++ profile.sh
@@ -12,10 +12,8 @@ fi
 
 cd ./$1
 
-if ! [[ -f ./main ]]; then
-    echo "Building in $1..."
-    nvcc -arch=sm_61 -DPROFILE=1 -o main main.cu neural_network.cu coordinates_dataset.cu nn_utils/*.cu layers/*.cu
-fi
+echo "Building in $1..."
+nvcc -arch=sm_61 -DPROFILE=1 -o main main.cu neural_network.cu coordinates_dataset.cu nn_utils/*.cu layers/*.cu
 
 # this one takes a while, so it can be ignored by passing a param
 if [[ -z "$2" ]]; then 
@@ -24,8 +22,8 @@ if [[ -z "$2" ]]; then
 fi
 
 echo "Profiling gpu-trace metrics in $1..."
-nvprof --csv --log-file profiler_trace_out_$1.csv --print-gpu-trace --print-api-trace ./main
-# nvprof --csv --log-file profiler_trace_out_$1.csv --print-gpu-trace ./main
+# nvprof --csv --log-file profiler_trace_out_$1.csv --print-gpu-trace --print-api-trace ./main
+nvprof --csv --log-file profiler_trace_out_$1.csv --print-gpu-trace ./main
 
 cd ..
 echo "Done!"
